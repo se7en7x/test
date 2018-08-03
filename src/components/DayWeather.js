98cdf49fc7cd30689f85b8198188f2aa5e84c125
Fen Cui <fencui@Fens-MacBook-Pro.local>
2018-08-03 11:13:18 -0400

    weather comp data

--- src/components/DayWeather.js
+++ src/components/DayWeather.js
@@ -11,7 +11,15 @@ class DayWeather extends Component{
 
 		const options = currentLocation.consolidated_weather.map(r => (
 			<li key={r.id}>
-				{r.weather_state_abbr}
+				<ul>
+					<li>{r.weather_state_abbr}</li>
+					<li>
+						<span>{r.max_temp}</span>
+						<span>{r.min_temp}</span>
+					</li>
+					<li>{r.applicable_date}</li>
+					<li>{r.weather_state_name}</li>
+				</ul>			
 			</li>
 		));
 
