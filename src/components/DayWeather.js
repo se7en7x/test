9fdc46a7db63c44b63e8515fee8d19ca877dbcd8
Steven Wang <steven.wang02@gmail.com>
2018-08-03 11:51:25 -0400

    dasdasd

--- src/components/DayWeather.js
+++ src/components/DayWeather.js
@@ -3,7 +3,21 @@ import PropTypes from "prop-types";
 
 class DayWeather extends Component{
 
-
+	weatherIco = weatherStateAbbr => {
+		let iconMapping = {
+			sn: "wi-day-snow",
+			sl: "wi-day-sleet",
+			h: "wi-day-hail",
+			t: "wi-day-thunderstorm",
+			hr: "wi-rain-wind",
+			lr: "wi-day-rain",
+			s: "wi-day-showers",
+			hc: "wi-day-cloudy",
+			lc: "wi-day-sunny-overcast",
+			c: "wi-day-sunny"
+		};
+		return "wi " + iconMapping[weatherStateAbbr];
+	};
 
 
 	render(){
@@ -12,14 +26,14 @@ class DayWeather extends Component{
 		const options = currentLocation.consolidated_weather.map(r => (
 			<li key={r.id}>
 				<ul>
-					<li>{r.weather_state_abbr}</li>
+					<li class={this.weatherIco(r.weather_state_abbr)}></li>
 					<li>
 						<span>{r.max_temp}</span>
 						<span>{r.min_temp}</span>
 					</li>
 					<li>{r.applicable_date}</li>
 					<li>{r.weather_state_name}</li>
-				</ul>			
+				</ul>
 			</li>
 		));
 
